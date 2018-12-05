6e7a8411b5c379dfef954165ed14a0317bfddf13
Oscar Wong <wong.oscar67@gmail.com>
2018-12-04 22:33:43 -0700

    FLUTTER

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Welcome to PipPup',
+        home: new Scaffold(
+            appBar: new AppBar(
+              title: const Text('Welcome to PipPup'),
+            ),
+            body: const Center(
+              child: const Text('Hello World!'),
+            )));
+  }
+}
