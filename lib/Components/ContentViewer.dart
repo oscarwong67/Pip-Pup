5423a12feba6896a944cc851eb5913ea89262655
Oscar Wong <wong.oscar67@gmail.com>
2019-01-13 22:02:33 -0700

    Update ContentViewer.dart

--- lib/Components/ContentViewer.dart
+++ lib/Components/ContentViewer.dart
@@ -161,7 +161,7 @@ class ContentViewerState extends State<ContentViewer> {
         controller.value.isPlaying) {
       controller.pause();
       controller.removeListener(_videoAudioListener);
-      controller.dispose();
+      controller.dispose();   //  TODO: move this to a more "safe" way
     }
   }
 
