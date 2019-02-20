0dcc3cfe4224534416c6c5f9e54bdc164c4e6467
Oscar Wong <wong.oscar67@gmail.com>
2019-02-20 14:17:49 -0700

    we stable now

--- lib/Components/ContentViewer.dart
+++ lib/Components/ContentViewer.dart
@@ -9,14 +9,112 @@ import 'package:flutter_advanced_networkimage/flutter_advanced_networkimage.dart
 import 'package:flutter_advanced_networkimage/transition_to_image.dart';
 
 import '../Classes/MediaObject.dart';
-
 class ContentViewerState extends State<ContentViewer> {
   List<MediaObject> mediaObjects = <MediaObject>[];
   final PageController pageController = new PageController();
   VideoPlayerController videoController;
+  ChewieController chewieVideoController;
   VideoPlayerController audioController;
+  ChewieController chewieAudioController;
   int currentIndex;
-  int currentPage;
+  int currentPage;  //  used to help track the index in the list of mediaobjects
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Welcome to PipPup!'),
+        actions: <Widget>[
+          new IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _debug,
+          )
+        ],
+      ),
+      body: _renderCurrentPage(),
+    );
+  }
+
+  Widget _renderCurrentPage() {
+    return new PageView.builder(
+        controller: pageController,
+        scrollDirection: Axis.vertical,
+        onPageChanged: (pageId) {
+          _movePage(pageId);
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return _renderCurrentContent(index);
+        },
+        itemCount: this.mediaObjects.length);
+  }  
+
+  //  render individual content page
+  Widget _renderCurrentContent(int index) {
+    if (index >= 0 &&
+        index < mediaObjects.length &&
+        mediaObjects[index] != null) {
+      Widget content;
+      MediaObject mediaObj = mediaObjects[index];
+
+      if (mediaObj.type == ContentType.IMAGE) {
+        content = _buildImage(mediaObj);
+      } else if (mediaObj.type == ContentType.GFY ||
+          mediaObj.type == ContentType.GIFV ||
+          mediaObj.type == ContentType.VIDEO) {
+        content = _buildVideo(mediaObj);
+      } else {
+        throw new Exception('Invalid Content Type! What the heck!');
+      }
+      return new Center(child: content);
+    } else {
+      return null;
+    }
+  }
+
+  Widget _buildImage(MediaObject mediaObj) {
+    return new TransitionToImage(
+          AdvancedNetworkImage(
+            mediaObj.url,
+            useDiskCache: false,
+            retryDuration: const Duration(milliseconds: 500),
+            retryLimit: 4,
+          ),
+          placeholder: const Icon(Icons.refresh),
+          loadingWidget:
+              const CircularProgressIndicator(), //  TODO: use the preview image link here?
+        );
+  }
+
+    // build a single video
+  Widget _buildVideo(MediaObject mediaObj) {
+    //  error check - in case elements are being pre-built and saved rather than built on-demand.
+    if (this.mediaObjects[this.currentIndex] == mediaObj) {
+      _cleanup(); //  clean up possible old controllers and stuff
+      _setupVideoControllers(mediaObj);
+      _setupChewieControllers(mediaObj);
+
+      return new Stack(
+        children: <Widget>[
+          new Chewie(
+            controller: chewieVideoController,
+          ),
+          //  invisible video player to play audio for reddit videos
+          (mediaObj.source == ContentSource.REDDIT &&
+                  this.audioController != null)
+              ? new Opacity(
+                  opacity: 0.0,
+                  child: new Chewie(
+                    controller: chewieAudioController
+                  ),
+                )
+              : null,
+          //  if the current source isn't reddit, then playing audio seperately isn't a problem, but "null" isn't a valid child widget value so filter it
+        ].where((child) => child != null).toList(), //  remove null children
+      );
+    } else {
+      return new CircularProgressIndicator(); //  TODO: put image placeholder or something
+    }
+  }
 
   //  go to next piece of content and handle if you need to go to the next chunk
   void _movePage(int pageId) {
@@ -35,105 +133,39 @@ class ContentViewerState extends State<ContentViewer> {
     });
   }
 
-  // build a single video
-  Widget _buildVideo(MediaObject mediaObj) {
-    //  error check - in case elements are being pre-built and saved rather than built on-demand.
-    if (this.mediaObjects[this.currentIndex] == mediaObj) {
-      if (this.videoController != null) {
-        this.videoController.dispose();
-      }
-      if (this.audioController != null) {
-        this.audioController.dispose();
-      }
-
-      this.videoController = new VideoPlayerController.network(mediaObj.url);
+  void _setupVideoControllers(MediaObject mediaObj) {
+    this.videoController = new VideoPlayerController.network(mediaObj.url);
       if (mediaObj.audioUrl != null && mediaObj.audioUrl.length > 0) {
         this.audioController =
             new VideoPlayerController.network(mediaObj.audioUrl);
-      } else {
-        this.audioController = null;
       }
       //  listen for when it's actually the current page
       this.videoController.addListener(_videoAudioListener);
-      return new Stack(
-        children: <Widget>[
-          new Chewie(
-            this.videoController,
-            autoPlay: true,
-            autoInitialize: true,
-            looping: true,
-            aspectRatio: mediaObj.width / mediaObj.height,
-            showControls: false,
-            placeholder: Center(
-                child:
-                    CircularProgressIndicator()), //  TODO: write custom controls that also pause/play the audio at the same time.
-          ),
-          //  invisible video player to play audio for reddit videos
-          (mediaObj.source == ContentSource.REDDIT &&
-                  this.audioController != null)
-              ? new Opacity(
-                  opacity: 0.0,
-                  child: new Chewie(
-                    this.audioController,
-                    autoPlay: false,
-                    autoInitialize: true,
-                    looping: true,
-                    showControls: false,
-                  ),
-                )
-              : null,
-          //  if the current source isn't reddit, then playing audio seperately isn't a problem, but "null" isn't a valid child widget value so filter it
-        ].where((child) => child != null).toList(), //  remove null children
+  }
+
+  void _setupChewieControllers(MediaObject mediaObj) {
+    this.chewieVideoController = ChewieController(
+        videoPlayerController: this.videoController,
+          autoPlay: true,
+          autoInitialize: true,
+          looping: true,
+          aspectRatio: mediaObj.width / mediaObj.height,
+          showControls: false, //  TODO: write custom controls that also pause/play the audio at the same time.
+          placeholder: Center(
+            child:
+              CircularProgressIndicator()
+          )
       );
-    } else {
-      return new CircularProgressIndicator(); //  TODO: put image placeholder or something
-    }
-  }
 
-  //  create a list of content of size CHUNK_SIZE
-  Widget _renderCurrentContent(int index) {
-    if (index >= 0 &&
-        index < mediaObjects.length &&
-        mediaObjects[index] != null) {
-      Widget content;
-      MediaObject mediaObj = mediaObjects[index];
-
-      if (mediaObj.type == ContentType.IMAGE) {
-        content = TransitionToImage(
-          AdvancedNetworkImage(
-            mediaObj.url,
-            useDiskCache: false,
-            retryDuration: const Duration(milliseconds: 500),
-            retryLimit: 4,
-          ),
-          placeholder: const Icon(Icons.refresh),
-          loadingWidget:
-              const CircularProgressIndicator(), //  TODO: use the preview image link here?
+      if (mediaObj.source == ContentSource.REDDIT && this.audioController != null) {
+        this.chewieAudioController = ChewieController(
+          videoPlayerController: this.audioController,
+          autoPlay: false,
+          autoInitialize: true,
+          looping: true,
+          showControls: false,
         );
-      } else if (mediaObj.type == ContentType.GFY ||
-          mediaObj.type == ContentType.GIFV ||
-          mediaObj.type == ContentType.VIDEO) {
-        content = _buildVideo(mediaObjects[index]);
-      } else {
-        throw new Exception('Invalid Content Type! What the heck!');
       }
-      return new Center(child: content);
-    } else {
-      return null;
-    }
-  }
-
-  Widget _renderCurrentPage() {
-    return new PageView.builder(
-        controller: pageController,
-        scrollDirection: Axis.vertical,
-        onPageChanged: (pageId) {
-          _movePage(pageId);
-        },
-        itemBuilder: (BuildContext context, int index) {
-          return _renderCurrentContent(index);
-        },
-        itemCount: this.mediaObjects.length);
   }
 
   void _videoAudioListener() {
@@ -163,6 +195,25 @@ class ContentViewerState extends State<ContentViewer> {
     return null;
   }
 
+  void _cleanup() {
+    if (this.videoController != null) {
+      this.videoController.dispose();
+    }
+    if (this.audioController != null) {
+      this.audioController.dispose();
+    }
+    if (this.chewieVideoController != null) {
+      this.chewieVideoController.dispose();
+    }
+    if (this.chewieAudioController != null) {
+      this.chewieAudioController.dispose();
+    }
+    this.videoController = null;
+    this.audioController = null;
+    this.chewieVideoController = null;
+    this.chewieAudioController = null;
+  }
+
   void _pauseIfNeeded(VideoPlayerController controller) {
     if (controller != null &&
         controller.value.initialized &&
@@ -180,19 +231,9 @@ class ContentViewerState extends State<ContentViewer> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Welcome to PipPup!'),
-        actions: <Widget>[
-          new IconButton(
-            icon: const Icon(Icons.list),
-            onPressed: _debug,
-          )
-        ],
-      ),
-      body: _renderCurrentPage(),
-    );
+  void dispose() {
+    super.dispose();
+    _cleanup();
   }
 
   void _debug() {
